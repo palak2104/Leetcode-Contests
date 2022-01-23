331496c77dece60ce13040c769aa2934b3fce683
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-01-23 18:51:28 +0530

    Update Count the Hidden Sequences.cpp

--- 22 Jan/Count the Hidden Sequences.cpp
+++ 22 Jan/Count the Hidden Sequences.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int numberOfArrays(vector<int>& d, int lower, int upper) {
-        long long sum = 0;
-long long mx =0 , mn = d[0];
-
-for(auto &i : d) {
-sum += i;
-mx = max(mx , sum);
-mn = min(mn , sum);
-}
-
-int ans = 0;
-
-for(int i = lower ; i <= upper ; i++) {
-if(i + mx <= upper && i + mn >= lower) {
-ans++;
-}
-}
-return ans;
-}
+    int numberOfArrays(vector<int>& arr, int lower, int upper) {
+      int sum=0;
+      int mini=0,maxi=0;
+      for(auto it:arr){
+          sum+=it;
+          mini=min(mini,sum);
+          maxi=max(maxi,sum);
+      }
+    int lb=lower-mini;
+    int ub=upper-maxi;
+    int temp=ub-lb+1;
+    if(temp>0) return temp;
+    else return 0;
+    }
 };
