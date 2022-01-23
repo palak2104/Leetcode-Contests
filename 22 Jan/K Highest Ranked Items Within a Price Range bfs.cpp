e1ff46b097369fee7a26b16fffbb34e6dc32f2e0
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-01-23 18:09:37 +0530

    Update K Highest Ranked Items Within a Price Range bfs.cpp

--- 22 Jan/K Highest Ranked Items Within a Price Range bfs.cpp
+++ 22 Jan/K Highest Ranked Items Within a Price Range bfs.cpp
@@ -11,43 +11,41 @@ public:
         int m = grid.size();
         int n =grid[0].size();
         vector<vector<int>>res;
-        vector<vector<bool>>vis(m+1,vector<bool>(n+1,false));
-        int i=start[0];
-        int j=start[1];
-        vis[i][j]=true;
+        vector<vector<bool>>vis(m,vector<bool>(n,false));
+        vis[start[0]][start[1]]=true;
         int a=pricing[0];
         int b=pricing[1];
         queue<pair<int,int>>q;
-        q.push({i,j});
+        q.push({start[0],start[1]});
         while(!q.empty()){
-            auto node=q.front();
-            q.pop();
-            i=node.first;
-            j=node.second;
-            if(grid[i][j]==0)
-            continue;
-            if(grid[i][j]>=1){
-            if(grid[i][j]>=a && grid[i][j]<=b && grid[i][j]!=1){
-                vector<int>temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                res.push_back(temp);
-            }
-           vector<int>x{-1,0,1,0};
-           vector<int>y{0,1,0,-1};
-           for(int r=0;r<4;r++){
-           if(isSafe(i+x[r],j+y[r],m,n) && vis[i+x[r]][j+y[r]]==false){
-           q.push({i+x[r],j+y[r]});
-           vis[i+x[r]][j+y[r]]==true;
-           }
-           }
-           }
+        if(k==0) break;
+        auto node=q.front();
             
+        q.pop();
+            
+        int i=node.first;
+        int j=node.second;
+            
+        if(grid[i][j]==0) continue;
+            
+        if(grid[i][j]>=a && grid[i][j]<=b){
+        res.push_back({i,j});
+        k--;
         }
-        vector<vector<int>>temp(k+1);
-        for(int i=0;i<k;i++)
-        temp[i]=res[i];
-        //func(grid,pricing[0],pricing[1],start[0],start[1],k,res,m,n,vis);
-        return temp;
+            
+        if(grid[i][j]>0){
+        vector<int>x{0,1,0,-1};
+        vector<int>y{1,0,-1,0};
+        for(int u=0;u<4;u++){
+        int p=i+x[u];
+        int r=j+y[u];
+        if(!isSafe(p,r,m,n)) continue;
+        if(vis[p][r]==true) continue;
+        vis[p][r]=true;
+        q.push({p,r});
+        }
+        }
+        }
+       return res; 
     }
 };
